8e8fd50aac19be3e26abc6963a998cca2dde2714
Micah N Gorrell <m@minego.net>
2026-02-17 15:53:49 -0800

    Added alternate hostnames for vaultwarden

--- modules/sites/minego.net/vaultwarden.nix
+++ modules/sites/minego.net/vaultwarden.nix
@@ -1,6 +1,19 @@
 # Secrets Management
 {
-	flake.modules.nixos.sites_minego_net = { config, ... }: {
+	flake.modules.nixos.sites_minego_net = { config, ... }: let
+		virtualHost = {
+			forceSSL					= true;
+
+			locations."/" = {
+				proxyPass				= "http://127.0.0.1:${toString config.services.vaultwarden.config.ROCKET_PORT}";
+				proxyWebsockets			= true;
+				extraConfig				= "proxy_pass_header Authorization;";
+			};
+
+			sslCertificate				= config.services.nginx.sslCertificate;
+			sslCertificateKey			= config.services.nginx.sslCertificateKey;
+		};
+	in {
 		services.vaultwarden = {
 			enable						= true;
 			dbBackend					= "sqlite";
@@ -13,18 +26,9 @@
 			};
 		};
 
-		services.nginx.virtualHosts."bitwarden.${config.services.nginx.hostname}" = {
-			forceSSL					= true;
-
-			locations."/" = {
-				proxyPass				= "http://127.0.0.1:${toString config.services.vaultwarden.config.ROCKET_PORT}";
-				proxyWebsockets			= true;
-				extraConfig				= "proxy_pass_header Authorization;";
-			};
-
-			sslCertificate				= config.services.nginx.sslCertificate;
-			sslCertificateKey			= config.services.nginx.sslCertificateKey;
-		};
+		services.nginx.virtualHosts."bitwarden.${config.services.nginx.hostname}"	= virtualHost;
+		services.nginx.virtualHosts."vaultwarden.${config.services.nginx.hostname}"	= virtualHost;
+		services.nginx.virtualHosts."secrets.${config.services.nginx.hostname}"		= virtualHost;
 	};
 }
 
